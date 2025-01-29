2c36b53936093af0a1dae15e57fffdaa9176de0f
JunoAndIce <ekwere.edi@gmail.com>
2025-01-28 18:16:03 -0600

    New Packages

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,11 +1,78 @@
 import hero_img from "../assets/images/hero_test.webp"
+import { useEffect, useRef } from "react";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import video from "../assets/video/ek.mp4"
+import Typed from 'typed.js';
+import React from 'react';
 
 function Hero() {
+
+
+    const component = useRef(null);
+    const el = useRef(null);
+
+    useEffect(() => {
+        let ctx = gsap.context(() => {
+            const tl = gsap.timeline({
+                scrollTrigger: {
+                    trigger: ".hero",
+                    pin: true,
+                    start: '+=10 top',
+                    end: 'bottom +=3000px',
+                    markers: true,
+                    scrub: 2,
+                }
+            })
+
+            tl.to(".hero-center h1", { duration: 12, opacity: 0, })
+                .to(".hero-center p", { duration: 6, opacity: 0 })
+                .to(".hero-link", { duration: 6, opacity: 0 })
+                .to(".videoTag", { duration: 6, opacity: 0 })
+
+            ScrollTrigger.refresh();
+        }, component);
+
+        return () => ctx.revert();
+    }, []);
+
+    React.useEffect(() => {
+        const typed = new Typed(el.current, {
+            strings: ['<i>Creative Director</i>', '<i>Graphic Designer</i>', 
+                        '<i>Photographer</i>','<i>Videographer</i>',' <i>Multidisciplinary</i>'],
+            typeSpeed: 50,
+            backSpeed: 50,
+            loop: true
+        });
+
+        return () => {
+            // Destroy Typed instance during cleanup to stop animation
+            typed.destroy();
+        };
+    }, []);
+
     return (
-        <div data-scroll-container className="hero">
-            <div className="hero_ctn">
-                <img className="hero_img" src={(hero_img)}></img>
+        <div ref={component} data-scroll-container className="hero items-end md:items-center">
+            {/* <div className="hero_ctn">
+                <img alt="" className="hero_img" src={(hero_img)}></img>
+            </div> */}
+
+            <video className=' videoTag' autoPlay muted loop playsInline>
+                <source src={video} type='video/mp4' />
+            </video>
+
+
+            <div className="hero_ctn flex flex-col gap-y-[250px] mb-5">
+                
+                <div className="flex flex-col items-center gap-10">
+                    <h1 className="text-8xl">Ekdsgn</h1>
+                    <div><span ref={el} className="typed"></span></div>
+                </div>
+                <div></div>
+                
             </div>
+
+
         </div>
     );
 }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,14 +1,15 @@
 import logo from "../assets/images/logo.webp"
 
+
 function Navbar() {
   return (
     <section data-scroll-sticky className='navbar'>
       <nav className="p-4 flex flex-row items-center">
-        <img className="p-2" src={(logo)}></img>
+        <img alt="" className="p-2" src={(logo)}></img>
 
         <span className="p-2 text-sm/1 basis-64 list-none font-thin text-sm">
           <li>Ekemini Ekwere</li>
-          <li>Based in USA, TX</li>
+          <li>Based in TX</li>
         </span>
       </nav>
     </section>
