185b4f10ba8894dc36a6bfb68edbd149fea3498f
JunoAndIce <ekwere.edi@gmail.com>
2025-01-31 16:09:23 -0600

    New fonts

--- src/components/menu.js
+++ src/components/menu.js
@@ -10,7 +10,7 @@ function Menu() {
 
     const { isMenuOpen } = useMenu();
     const menuRef = useRef(null);
-    
+
     // GSAP vars for animating multiple items at once.
     const listItemsRef = useRef([]); // Ref for individual list items
     const socialListRef = useRef(null); // Ref for the entire social list
@@ -19,7 +19,7 @@ function Menu() {
     useEffect(() => {
         if (isMenuOpen) {
             document.body.style.overflow = "hidden"; // Disable scrolling
-    
+
             gsap.to(menuRef.current, {
                 opacity: 1,
                 y: 0,
@@ -27,7 +27,7 @@ function Menu() {
                 ease: "expo.out",
                 display: "block",
             });
-    
+
             gsap.from(listItemsRef.current, {
                 opacity: 0,
                 y: 20,
@@ -35,7 +35,7 @@ function Menu() {
                 ease: "power3.out",
                 stagger: 0.1,
             });
-    
+
             gsap.from(socialListRef.current, {
                 opacity: 0,
                 y: 20,
@@ -43,10 +43,10 @@ function Menu() {
                 ease: "power3.out",
                 delay: 0.3,
             });
-    
+
         } else {
             document.body.style.overflow = ""; // Re-enable scrolling
-    
+
             gsap.to(menuRef.current, {
                 opacity: 0,
                 y: -20,
@@ -59,7 +59,7 @@ function Menu() {
                 },
             });
         }
-    
+
         // Cleanup when component unmounts
         return () => {
             document.body.style.overflow = "";
@@ -72,28 +72,36 @@ function Menu() {
         <section ref={menuRef} className={`${isMenuOpen ? "block" : "hidden"} menu flex justify-center items-center fixed z-10 w-screen h-screen m-auto`}>
 
             <div className='flex justify-between flex-col w-screen h-screen p-6'>
+                <div className='flex mt-[140px] gap-2'>
+                    <FontAwesomeIcon className='mt-3' icon={faCircle} size='2xs' />
+                    <ul className="flex flex-col ">
 
-                <ul className="flex gap-10 flex-col uppercase mt-[140px]">
+                        {/* Animated list items */}
+                        {["Founder", "Creative Director", "Cinemtographer", "Photographer", "Graphic Designer"].map((item, index) => (
+                            <li key={index} ref={(el) => (listItemsRef.current[index] = el)} className="text-3xl uppercase">
+                                {item}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
 
-                    <li><FontAwesomeIcon icon={faCircle} size='2xs' /></li>
+                <div className='flex gap-2'>
+                    <FontAwesomeIcon className='mt-3' icon={faCircle} size='2xs' />
+                    <ul>
+                        <li className="text-3xl uppercase">About Me</li>
+                    </ul>
+                </div>
 
-                    {/* Animated list items */}
-                    {["Photography", "Videography", "Creative Direction", "Brands", "About Me"].map((item, index) => (
-                        <li key={index} ref={(el) => (listItemsRef.current[index] = el)} className="text-3xl">
-                            {item}
-                        </li>
-                    ))}
+                <div>
+                    <ul ref={socialListRef} className='flex gap-[15vw] justify-center'>
+                        <li><FontAwesomeIcon icon={faXTwitter} /></li>
+                        <li><FontAwesomeIcon icon={faInstagram} /></li>
+                        <li><FontAwesomeIcon icon={faTiktok} /></li>
+                        <li><FontAwesomeIcon icon={faLinkedin} /></li>
+                        <li><FontAwesomeIcon icon={faFacebook} /></li>
+                    </ul>
+                </div>
 
-                    <li><FontAwesomeIcon icon={faCircle} size='2xs' /></li>
-                </ul>
-
-                <ul ref={socialListRef} className='flex gap-[15vw] justify-center'>
-                    <li><FontAwesomeIcon icon={faXTwitter} /></li>
-                    <li><FontAwesomeIcon icon={faInstagram} /></li>
-                    <li><FontAwesomeIcon icon={faTiktok} /></li>
-                    <li><FontAwesomeIcon icon={faLinkedin} /></li>
-                    <li><FontAwesomeIcon icon={faFacebook} /></li>
-                </ul>
             </div>
         </section>
 
