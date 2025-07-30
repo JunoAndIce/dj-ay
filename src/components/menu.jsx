dd01ab28835e536a4e146485b85ee57ecd0a716b
JunoAndIce <ekwere.edi@gmail.com>
2025-07-30 14:36:39 -0500

    Navbar changes New navbar items for PC have been added and hid the hamburger menu for PC.

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -55,8 +55,8 @@ const Menu = () => {
         <Link to='/dj-ay'><li>Home</li></Link>
         <Link to='/dj-ay/about-me'><li>About Me</li></Link>
         <Link to='/dj-ay/news'><li>News</li></Link>
-        <Link to='/dj-ay/my-music'><li>Music</li></Link>
-        <Link onClick={() => window.location.href = 'mailto:dja.yofficial2@gmail.com'}><li>Book</li></Link>
+        <Link to='/dj-ay/my-music'><li>My Music</li></Link>
+        <Link onClick={() => window.location.href = 'mailto:dja.yofficial2@gmail.com'}><li>Contact Me</li></Link>
       </ul>
     </aside>
   );
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -44,15 +44,21 @@ const Navbar = () => {
         <div className='navbar-brand justify-between w-full px-4 py-2'>
           <Link className='navbar-item' to='/dj-ay'>
             <img
-              className={`h-10 transition-opacity duration-300 ease-in-out ${
-                isMenuOpen ? 'opacity-0' : 'opacity-100'
-              }`}
+              className={`h-10 transition-opacity duration-300 ease-in-out ${isMenuOpen ? 'opacity-0' : 'opacity-100'
+                }`}
               src={logo}
               alt='Logo'
             />
           </Link>
+          <div class="navbar-start is-hidden-touch px-4 text-lg ">
+            <Link to='/dj-ay'><a className='navbar-item ml-5'>Home</a></Link>
+            <Link to='/dj-ay/about-me'><a className='navbar-item ml-5'>About Me</a></Link>
+            <Link to='/dj-ay/news'><a className='navbar-item ml-5'>News</a></Link>
+            <Link to='/dj-ay/my-music'><a className='navbar-item ml-5'>My Music</a></Link>
+            <Link onClick={() => window.location.href = 'mailto:dja.yofficial2@gmail.com'}><a className='navbar-item ml-5'>Contact Me</a></Link>
+          </div>
 
-          <div className='navbar-end'>
+          <div className='navbar-end is-hidden-tablet'>
             <div className='navbar-item'>
               <Hamburger toggled={isMenuOpen} toggle={toggleMenu} />
             </div>
